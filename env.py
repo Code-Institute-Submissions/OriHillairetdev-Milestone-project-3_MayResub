da2e568f7a4cf2f76179a19e96014c60b9056ede
OriHillairetdev <Wolfhilliaret@gmail.com>
2021-05-14 15:06:49 +0000

    removed secret key from env.py

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ import os
 
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "MagicalCatMan")
+os.environ.setdefault("SECRET_KEY")
 os.environ.setdefault(
     "MONGO_URI",
     "mongodb+srv://Ori_dev:Wolf777@book-nook.ofwaf.mongodb.net/BookNook \
