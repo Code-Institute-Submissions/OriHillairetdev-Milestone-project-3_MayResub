87182b355306b60d4745106a0587992129c6c756
OriHillairetdev <Wolfhilliaret@gmail.com>
2021-05-14 15:11:07 +0000

    fixed line length of line 8 in env,py for pEP8

--- env.py
+++ env.py
@@ -5,5 +5,6 @@ os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "MagicalCatMan")
 os.environ.setdefault(
     "MONGO_URI",
-    "mongodb+srv://Ori_dev:Wolf777@book-nook.ofwaf.mongodb.net/BookNook?retryWrites=true&w=majority")
+    "mongodb+srv://Ori_dev:Wolf777@book-nook.ofwaf.mongodb.net/ \
+        BookNook?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "BookNook")
